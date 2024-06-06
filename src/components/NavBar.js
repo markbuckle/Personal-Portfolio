017ad8212342db3024ba984752d8e241fdbc2d39
Mark Buckle <markbuckle92@gmail.com>
2024-06-06 13:03:51 -0230

    Commit #5

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,7 +9,6 @@ import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 
-
 // use a function export component
 export const NavBar = () => {
 
